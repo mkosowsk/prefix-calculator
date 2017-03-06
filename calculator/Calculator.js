a7e5b2e2f235a115f8290436c88e8723c2b77dc6
Mitch Kosowski <mitchkosowski@gmail.com>
2017-03-05 19:42:51 -0500

    changed Calculator to Class

--- calculator/Calculator.js
+++ calculator/Calculator.js
@@ -1,46 +1,41 @@
-function Calculator() {
-  this.numStack = [];
+class Calculator{
+	constructor(){
+		this.numStack = [];
+	}
+
+	push(num){
+		this.numStack.push(num);
+	}
+
+	compute(operationFunction){
+		if (this.numStack.length < 2) {
+		  throw 'Calculator is empty';
+		}
+		const num1 = this.numStack.pop();
+		const num2 = this.numStack.pop();
+		this.push(operationFunction(num1,num2));
+	}
+
+	add(){
+		this.compute((num1,num2) => num1+num2);
+	}
+
+	multiply(){
+		this.compute(function(first,second){
+			return first*second;
+		});
+	}
+
+	divide(){
+	  this.compute(function(first,second){
+		// throw error on divide by 0
+	  	return second / first;
+	  });
+	}
+
+	value(){
+		return this.numStack[this.numStack.length-1];
+	}
 }
-
-Calculator.prototype.push = function(num) {
-  this.numStack.push(num);
-};
-
-Calculator.prototype.popCompute = function(operationFunc){
-    if (this.numStack.length < 2) {
-      throw "Calculator is empty";
-    }
-    const num1 = this.numStack.pop();
-    const num2 = this.numStack.pop();
-    this.push(operationFunc(num1,num2));
-};
-
-Calculator.prototype.plus = function() {
-	this.popCompute((num1,num2) => num1+num2);
-};
-
-Calculator.prototype.value = function() {
-  return this.numStack[this.numStack.length-1];
-};
-
-Calculator.prototype.minus = function() {
-  this.popCompute(function(first, second){
-  	return second - first;
-  });
-};
-
-Calculator.prototype.times = function() {
-	this.popCompute(function(first,second){
-		return first*second;
-	});
-};
-
-Calculator.prototype.divide = function() {
-  this.popCompute(function(first,second){
-	// throw error on divide by 0
-  	return second / first;
-  });
-};
-
 
 module.exports = Calculator;
--- functions/runCalculator.js
+++ functions/runCalculator.js
@@ -2,11 +2,10 @@ const runCalculator = (calculator, operatorArr, operandArr) => {
 	for (let i = operandArr.length - 1; i >= 0; i--){
 		calculator.push(operandArr[i]);
 	}
-	// console.log(calculator.numStack);
 	for (let j = operatorArr.length - 1; j >= 0; j--){
-		if (operatorArr[j] === '+') calculator.plus();
+		if (operatorArr[j] === '+') calculator.add();
 		if (operatorArr[j] === '-') calculator.minus();
-		if (operatorArr[j] === '*') calculator.times();
+		if (operatorArr[j] === '*') calculator.multiply();
 	}
 	return calculator.value();
 }
